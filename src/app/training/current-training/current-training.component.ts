c66d2b8e7d35f39385b102db9ae914e1d9580757
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-12-05 19:39:29 -0500

    stop current training components implementation with modal

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material';
 
 import { StopTrainingComponent } from './stop-training.component'
@@ -9,18 +9,24 @@ import { StopTrainingComponent } from './stop-training.component'
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() trainingExit = new EventEmitter();
   progress = 0;
   timer: number;
 
   constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
+    this.startOrResumeTimer();
+  }
+
+  startOrResumeTimer() {
     this.timer = setInterval(() => {
       this.progress = this.progress + 10;
       if(this.progress >= 100) {
         clearInterval(this.timer);
       }
     }, 1000)
+
   }
 
   onStop() {
@@ -32,7 +38,13 @@ export class CurrentTrainingComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
+      if (result) {
+        this.trainingExit.emit();
+      } else {
+        if(this.progress < 100) {
+          this.startOrResumeTimer();
+        }
+      }
     });
   }
 }
